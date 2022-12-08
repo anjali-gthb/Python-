954db85714303ca4931393b19b5474cb1a00950e
anjali <110936462+anjali-gthb@users.noreply.github.com>
2022-12-08 17:47:16 +0530

    Delete escape.py

--- escape.py
+++
@@ -1,9 +0,0 @@
-#output : lineA \n lineB
-print("line A \\n line B")     #(\\)-->\
-print("line B \\t line B")    #(\\)--> \
-print("this is 4 backslash \\\\\\\\")
-
-#output:- \" \'
-print ("\\\" \\\' ")
-# \' = '
-#\\  = \
